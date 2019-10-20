0df21227c8afa3b1d8f0baec7d116c95f0ef1829
Robert Strauss <robert.strauss@pm.me>
2019-10-20 16:14:09 -0600

    fixed preview, css updates

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-let hue = 85;
+// rainbow background
+let hue = 210;
 setInterval(function(){
   hue = (hue-1)%360;
   document.documentElement.style.setProperty('--main-bg',  `hsl(${hue}, 50%, 60%)`);
-}, 50);
+}, 100);
+
+// modals
+const query = document.querySelectorAll.bind(document);
+
+let modalRoot = query('.modal');
+let modals = query('.modal-content');
+
+modalRoot.forEach(modalr => modalr.addEventListener('click', function () {
+  modalr.style.display = 'none';
+}));
+
+modals.forEach(modal => modal.addEventListener('click', function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  e.stopImmediatePropagation();
+  return false;
+}));
+
+
+
+
+
 
 // socket to communicate with server
 let socket = io();
@@ -16,6 +39,10 @@ thisNode.pubkey = RegExp('pubkey=([^;]+)').exec(document.cookie);
 
 const difficulty = 4; // how many zeros (hex) block hash must start with
 
+const maxbackfork = 20; // the furthest number of blocks back a fork can be started
+
+
+
 
 
 
@@ -28,18 +55,32 @@ request.onupgradeneeded = function(e) { // called if the user doesn't have a blo
   console.log('initializing blockchain database');
   blockchaindb = request.result; // global way of accessing blockchain
 
-  // create a 'objectstore' in the database - where all the data actually is
-  let objectStore = blockchaindb.createObjectStore('blockchain', { keyPath: 'index' });
-  objectStore.createIndex('index', 'index', { unique: true });
-  objectStore.createIndex('previoushash', 'previoushash', { unique: true });
-  objectStore.createIndex('transactions', 'transactions', { unique: false });
-  objectStore.createIndex('proofofwork', 'proofofwork', { unique: false });
+  // create objectstore for the full blockchain
+  let bcObjectStore = blockchaindb.createObjectStore('blockchain', { keyPath: 'hash' });
+  bcObjectStore.createIndex('prevhash',    'prevhash',    { unique: true  });
+  bcObjectStore.createIndex('transactions','transactions',{ unique: false });
+  bcObjectStore.createIndex('proofofwork', 'proofofwork', { unique: false });
 
-  console.log('created object store');
+  console.log('created blockchain object store');
+
+  // object store for just the most recent blocks
+  let ebObjectStore = blockchaindb.createObjectStore('endblocks', { keyPath: 'hash' });
+  ebObjectStore.createIndex('prevhash',    'prevhash',    { unique: true  });
+  ebObjectStore.createIndex('transactions','transactions',{ unique: false });
+  ebObjectStore.createIndex('proofofwork', 'proofofwork', { unique: false });
+
+  console.log('created endblocks object store');
+
+  // genesis block
+  bcObjectStore.add({hash: '0000684b7c4cc4bd581d92f7be29d4830d3bd66e320f885900cc7f537e1408cc',
+                     prevhash: '', transactions:'strobertisawesome>100>strobert', proofofwork: '44124'});
+  ebObjectStore.add({hash: '0000684b7c4cc4bd581d92f7be29d4830d3bd66e320f885900cc7f537e1408cc',
+                     prevhash: '', transactions:'strobertisawesome>100>strobert', proofofwork: '44124'});
   console.log('database initialization complete');
-  // blockchain objectstore should be empty
-  // right now, but it will be filled
-  // when onsuccess is called after this
+  /**
+   * blockchain will be synced
+   * when onsuccess is called after this
+   */
 }
 request.onsuccess = function() {
   blockchaindb = request.result; // set the global variable for accessing blockchain
@@ -64,35 +105,61 @@ request.onsuccess = function() {
 
 
 
-
+// when a transaction is recieved
 socket.on('transaction', function(transactionstring){
-  // TODO verify transaction, mine it (optional), add it to local blockchain
+  // TODO verify transaction, mine it. (optional: if mining)
   console.log('received transaction: ', transactionstring);
 });
-socket.on('block', function(blockstring){
-  console.log('received block: ', blockstring);
 
-  if (!SHA256(blockstring).startsWith(Array(difficulty+1).join('0'))) // check if hash of block starts with zeros according to difficulty
-    return false; // if not, reject
+// when a block is recieved
+socket.on('block', function(blockstring){
+  let split = blockstring.split(';');
+  let newblock = {hash: SHA256(blockstring), prevhash: split[0], transactions: split[1], proofofwork: split[2]};
+  console.log('received block: ', newblock);
+
+  if (!newblock.hash.startsWith(Array(difficulty+1).join('0'))) // check if hash of block starts with zeros according to difficulty
+    return; // if not, reject
 
   console.log('Block matches difficulty');
 
-  let prevhash = blockstring.split(';')[0]; // the head of the block linking it to the last block
-  // go through the last 10 blocks to check if it appends to any of them
-  let blockchainos = blockchaindb.transaction(['blockchain'], 'readonly').objectStore('blockchain');
-  let count = 0;
-  blockchainos.openCursor(null, 'prev').onsuccess = function(event) { // iterate backwards by index
-    let cursor = event.target.result;
-    if (count < 10 && cursor) { // if within the last 10 blocks (after 10, no forks allowed)
-      if(cursor.value.previoushash === prevhash){ // if the block says it comes after this one
-        console.log('block can be inserted at index: ' + (parseInt(cursor.value.index)+1)); // save the index of the block it comes after
-      }
-      else {
-        count++;
-        cursor.continue(); // otherwise continue search
-      }
+  // get the blockchain and endblocks ready to read from and write to
+  let transaction = blockchaindb.transaction(['blockchain', 'endblocks'], 'readwrite');
+  let blockchainos = transaction.objectStore('blockchain'); // full blockchain
+  let endblockos = transaction.objectStore('endblocks') // the most recent blocks
+  console.log('created transacion');
+  /*
+  #######################################################
+  #### the blockchain is self-organizing because     ####
+  #### each block has the hash of the previous block.####
+  #### we only need to determine if a block should   ####
+  #### be added, not where to put it.                ####
+  #######################################################
+  */
+
+  // check if the new block connects to any previous block recent enough to allow forks
+  endblockos.openCursor().onsuccess = function(e){
+    let endblock = e.target.result.value;
+    // check if the new block extends an endblock
+    if (newblock.prevhash === endblock.hash) {
+      console.log('accepting block ', newblock);
+      endblocks.delete(endblock); // end block is no longer end block
+      endplocks.add(newblock); // new block is
+      blockchainos.add(newblock); // add new block to blockchain
+      return true; // exit loop
     }
-  }
+    // check if the new block extends any block within the last <maxbackfork> blocks, starting a new fork.
+    for (let backcount=0, lastblock=endblock; backcount < maxbackfork; backcount++) {
+      blockchainos.get(lastblock.prevhash).onsuccess = function(e) {
+        lastblock = e.target.result.value;
+        if (newblock.prevhash === lastblock.hash) {
+          console.log('accepting block ', newblock);
+          endblocks.add(newblock);
+          blockchainos.add(newblock); // add new block to blockchain
+          return true; // exit loop
+        }
+      };
+    }
+  };
 
 });
 
@@ -100,7 +167,7 @@ socket.on('block', function(blockstring){
 
 
 // preview blockchain after 100ms to give async functions time to fill it
-setTimeout(previewBlockChain, 100);
+setTimeout(previewBlockchain, 100);
 
 
 
@@ -144,7 +211,7 @@ function broadcasttransaction(amount, recipient) {
   let sign = new JSEncrypt();
   sign.setPrivateKey(thisNode.privkey);
   let signature = sign.sign(transactionstring);
-  // cryptico doesn't work with signing, that's why the other package is implemented too
+  // cryptico doesn't work with signing, that's why the other packalet cursorrequest = ge is implemented too
   // let signature = cryptico.decrypt(transactionstring, thisNode.privkey); // encrypting with private key is signing
   console.log(signature);
   socket.emit('transaction', transactionstring + '|' + signature);
@@ -156,37 +223,38 @@ function broadcasttransaction(amount, recipient) {
 
 
 
-function previewBlockChain() {
-
-  let blockchainos = blockchaindb.transaction(['blockchain'], 'readonly').objectStore('blockchain');
+function previewBlockchain() {
+  let transaction  = blockchaindb.transaction(['blockchain', 'endblocks'], 'readonly');
+  let blockchainos =  transaction.objectStore('blockchain');
+  let endblockos   =  transaction.objectStore('endblocks' );
 
   // fill the blockchain preview div
   blockchainpreviewdiv = document.getElementById('blockchainpreview');
 
-  let cursorrequest = blockchainos.openCursor(null, 'prev'); // iterate in backwards direction (prev) to show last blocks
-
-  let count = 0; // count of how many blocks back to preview
-  cursorrequest.onsuccess = function(e) {
+  endblockos.openCursor().onsuccess = function(e) { // once for each fork
     let cursor = e.target.result;
-    if (count < 10 && cursor) {
+    if (cursor) {
+      let endblock = cursor.value;
+      console.log(endblock);
+      for (let i = 0, block=endblock; i < maxbackfork; i++) { // only display as many back as can be forked
+        blockchainos.get(block.prevhash).onsuccess = function(e) {
+          let blockdiv = document.createElement('a'); // create an element for this block
+          blockdiv.className = 'block'; // of class block
+          blockdiv.href = '/blockchain/'+block.index; // that links to a page on the block
 
-      let block = cursor.value;
+          // create individual divs for the previous hash, transactions, and proofofwork of the block
+          ['prevhash', 'transactions', 'proofofwork'].forEach(function(key){
+            let element = document.createElement('div');
+            element.className = key;
+            element.innerHTML = block[key];
+            blockdiv.appendChild(element);
+          });
 
-      let blockdiv = document.createElement('a'); // create an element for this block
-      blockdiv.className = 'block'; // of class block
-      blockdiv.href = '/blockchain/'+block.index; // that links to a page on the block
+          blockchainpreviewdiv.appendChild(blockdiv); // add created html block to the document
 
-      // create individual divs for the previous hash, transactions, and proofofwork of the block
-      Object.keys(block).forEach(function(key){
-        let element = document.createElement('div');
-        element.className = key;
-        element.innerHTML = block[key];
-        blockdiv.appendChild(element);
-      });
-
-      blockchainpreviewdiv.appendChild(blockdiv); // add created html block to the document
-
-      count++;
+          block = e.target.result.value;
+        }
+      }
       cursor.continue();
     }
   };
