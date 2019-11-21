afbc7801c687016e14d18735342f1ce61117e488
ROBERT STRAUSS <robertstrauss@LAHS-COMPSCI18-05.local>
2019-11-21 15:28:08 -0700

    save

--- style.js
+++ style.js
@@ -7,11 +7,11 @@ lightdarkmode.addEventListener('click', function() {
       getComputedStyle(docelem).getPropertyValue('--bg-clr-dark').trim()) {
         docelem.style.setProperty('--bg-clr',  getComputedStyle(docelem).getPropertyValue('--bg-clr-light'));
         docelem.style.setProperty('--text-clr',  getComputedStyle(docelem).getPropertyValue('--text-clr-light'));
-        lightdarkmode.innerHTML = '☀ Light Mode'; // ☽
+        lightdarkmode.innerHTML = '<img src="sun.svg"/> Light Mode'; // ☽
   } else {
     docelem.style.setProperty('--bg-clr',  getComputedStyle(docelem).getPropertyValue('--bg-clr-dark'));
     docelem.style.setProperty('--text-clr',  getComputedStyle(docelem).getPropertyValue('--text-clr-dark'));
-    lightdarkmode.innerHTML = '🌔 Dark Mode'; // ☼  ❂
+    lightdarkmode.innerHTML = '<img src="moon.svg"/> Dark Mode'; // ☼  ❂  🌔
   }
 });
 
